39fe85aca7c449e46a57c15c7386a8cc3a86d88a
hieule <lehieuminh231@gmail.com>
2019-06-23 10:54:48 +0700

    doc 22-06

---
+++ Document/VueJS/Example/main.js
@@ -0,0 +1,16 @@
+new Vue({
+        el: '#app',
+        data: {
+        submissions: Seed.submissions
+    },
+    computed: {
+        sortedSubmissions () {
+        return this.submissions.sort((a, b) => {
+            return b.votes - a.votes
+        });
+        }   
+    },
+    components: {
+        'submission-component': submissionComponent
+    }
+});
